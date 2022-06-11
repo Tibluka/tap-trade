3e4040edca15670c39f896b61aa0f94874a70f07
Tibluka <cprpc@hotmail.com>
2022-06-11 17:31:46 -0300

    .

--- src/app/services/account.service.ts
+++ src/app/services/account.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -6,15 +8,21 @@ import { Injectable } from '@angular/core';
 export class AccountService {
 
   private accountMode: string = 'PRACTICE';
+  private accountBalance: any;
 
   get mode() {
     return this.accountMode;
   }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   setAccountMode(mode: string) {
     this.accountMode = mode;
+  }
+
+  async setAccountBalance() {
+    const balance = await this.http.get(`${environment.url}`).toPromise()
+    this.accountBalance = balance;
   }
 
 }
